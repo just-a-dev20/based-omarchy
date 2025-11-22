efc20a94a75462b0f724f86665a22a644dbc2c9d
justaguy <215409593+just-a-dev20@users.noreply.github.com>
2025-11-22 15:24:13 +0100

    Add system update command before package installation

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,3 +1,4 @@
 # Install all base packages
 mapfile -t packages < <(grep -v '^#' "$OMARCHY_INSTALL/omarchy-base.packages" | grep -v '^$')
+sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm --needed "${packages[@]}"
